b93c54f515747c0e638f46e4ac2cece7d5992a9e
nirvanacc <1970681125@qq.com>
2018-09-21 17:33:50 +0800

    申请处理页面  列表

--- src/components/applicant.vue
+++ src/components/applicant.vue
@@ -15,26 +15,21 @@
                 <el-button type="primary" @click="search">{{"搜索"}}</el-button>
               </el-form-item>
               <el-form-item style="margin-top:7px">
-                <el-button type="primary" @click="addServer">{{"新增"}}</el-button>
-              </el-form-item>
-              <el-form-item style="margin-top:7px">
-                <el-button type="primary" @click="deleteBatch">{{"批量删除"}}</el-button>
+                <el-button type="primary" @click="addServer">{{"历史申请"}}</el-button>
               </el-form-item>
             </el-form>
           </div>
-          <el-table :data="serverList" @selection-change="handleSelectionChange" style="width:100%;margin-top:10px">
-            <el-table-column type="selection" width="55" align="center"></el-table-column>
+          <el-table :data="serverList" style="width:100%;margin-top:10px">
             <el-table-column type="index" width="60" align="center"></el-table-column>
-            <el-table-column prop="name" label="名称" width="200" align="center"></el-table-column>
-            <el-table-column prop="os" label="系统" width="200" align="center"></el-table-column>
-            <el-table-column prop="runningStateText" label="运行状态" width="200" align="center" sortable></el-table-column>
-            <el-table-column prop="isAllocatedText" label="是否分配" width="200" align="center" sortable></el-table-column>
-            <el-table-column prop="ownerText" label="所有者" align="center" width="200"></el-table-column>
-            <el-table-column prop="modifiedDate" label="上次编辑于" align="center" width="200"></el-table-column>
+            <el-table-column prop="detail" label="概述" width="400" align="center"></el-table-column>
+            <el-table-column prop="applicantText" label="申请人" width="200" align="center"></el-table-column>
+            <el-table-column prop="requestDate" label="申请日期" width="200" align="center" sortable></el-table-column>
+            <el-table-column prop="stateText" label="状态" width="200" align="center" sortable></el-table-column>
+            <el-table-column prop="operatorText" label="经办人" align="center" width="200"></el-table-column>
+            <el-table-column prop="operatorDate" label="操作日期" align="center" width="200"></el-table-column>
             <el-table-column label="操作" align="center">
               <template slot-scope="scope">
-                <el-button @click="editServer(scope.$index, scope.row)" type="primary" size="small">详情</el-button>
-                <el-button @click="deleteServer(scope.$index, scope.row)" type="primary" size="small">删除</el-button>
+                <el-button @click="editServer(scope.$index, scope.row)" type="primary" size="small">处理</el-button>
               </template>
             </el-table-column>
           </el-table>
@@ -47,46 +42,16 @@
       </el-col>
     </el-row>
     <Footer></Footer>
-    <!-- 编辑服务器信息 -->
-    <el-dialog :title="title" :visible.sync="editVisible" width="25%" :center="isCenter">
-      <el-form :model="server" label-position="right" label-width="80px">
-        <el-form-item label="名称">
-          <el-input v-model="server.name" placeholder="服务器名称 \ 代号"></el-input>
-        </el-form-item>
-        <el-form-item label="搭载系统">
-          <el-input v-model="server.os" placeholder="服务器系统"></el-input>
-        </el-form-item>
-        <el-form-item label="运行状态">
-          <el-select v-model="server.runningState" placeholder="请选择">
-            <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="分配给">
-          <el-input placeholder="添加设备所有人" ref="ownerName" v-model="server.ownerText">
-            <el-button slot="append" icon="el-icon-circle-plus" @click="addOwner"></el-button>
-            <el-button slot="append" icon="el-icon-remove" @click="deleteOwner"></el-button>
-          </el-input>
-        </el-form-item>
-        <el-form-item label="最近的编辑人" v-show="editShow">
-          <el-input v-model="server.modifiedByText" disabled></el-input>
-        </el-form-item>
-        <el-form-item label="编辑时间" v-show="editShow">
-          <el-input v-model="server.modifiedDate" disabled></el-input>
+    <el-dialog title="处理申请" :visible.sync="handlerVisible" width="19%" top="15vh" :center="isCenter">
+      <el-form :model="requestInfo" label-position="top" label-width="80px">
+        <el-form-item label="申请详情">
+          <el-input type="textarea" :rows="2" placeholder="请输入内容" v-model="requestInfo.detail"></el-input>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
-        <el-button @click="editVisible = false">取 消</el-button>
-        <el-button type="primary" @click="editSubmit">确 定</el-button>
+        <el-button @click="applyVisible = false">取 消</el-button>
+        <el-button type="primary" @click="applySubmit">确 定</el-button>
       </span>
-      <!-- 选择添加设备所有人 -->
-      <el-dialog title="分配给" :visible.sync="allocateVisible" append-to-body width="19%" :center="isCenter">
-        <el-input v-model="searchConsumerInput"></el-input>
-        <el-table :data="consumerList" @row-click="rowClick" border style="width:311px">
-          <el-table-column type="index" width="60" align="center"></el-table-column>
-          <el-table-column prop="name" label="用户名" width="100" align="center"></el-table-column>
-          <el-table-column prop="phone" label="电话" width="150" align="center"></el-table-column>
-        </el-table>
-      </el-dialog>
     </el-dialog>
   </div>
 </template>
@@ -110,6 +75,11 @@ export default {
           totalRows: 0
         },
         pageSizes: [5, 10],
+        isCenter: true,
+
+        requestInfoList: [],
+        handlerVisible: false,
+
         serverList: [],
         consumerList: [],
         server: {
@@ -128,8 +98,7 @@ export default {
           label: '异常'
         }],
         deleteBatchList: [],
-        searchConsumerInput: '',
-        isCenter: true
+        searchConsumerInput: ''
       }
     },
     // 分页的计算属性
@@ -156,7 +125,7 @@ export default {
        this.deleteBatchList = val;
       },
       getServerList() {
-        this.$api.get('server/pageAll?page='+this.page.pageNum+'&size='+this.page.size, null, r => {
+        this.$api.get('requestInfo/pageAll?page='+this.page.pageNum+'&size='+this.page.size, null, r => {
           this.serverList = r.data.content;
           this.page.totalRows = r.data.totalElements;
         })
